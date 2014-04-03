3efb1ed7751a2f098d25414387527959a0c0bed8
rodrigo-bruno <rodrigo.bruno@ist.utl.pt>
2014-04-03 01:23:44 +0100

    New simple_app implementation (simplified version of uc2).

--- src/simple_app.cpp
+++ src/simple_app.cpp
@@ -1,6 +1,6 @@
 // This file is part of BOINC.
 // http://boinc.berkeley.edu
-// Copyright (C) 2013 University of California
+// Copyright (C) 2008 University of California
 //
 // BOINC is free software; you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License
@@ -15,75 +15,64 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
-// another BOINC test application;
-// converts one or more files to uppercase,
-// then uses some CPU time
+// The program converts a mixed-case file to upper case:
+// read "in", convert to upper case, write to "out"
 //
-// cmdline: in1 out1 ... inn outn
+// command line options
+// --cpu_time N: use about N CPU seconds after copying files
 //
 
-#include <stdio.h>
+#include "boinc/config.h"
+#include <cstdio>
+#include <cctype>
+#include <ctime>
+#include <cstring>
+#include <cstdlib>
+#include <csignal>
+#include <unistd.h>
 
-#include "boinc/boinc_api.h"
-#include "boinc/filesys.h"
+#include "boinc/str_util.h"
 #include "boinc/util.h"
+#include "boinc/filesys.h"
+#include "boinc/boinc_api.h"
+#include "boinc/mfile.h"
+
+using std::string;
+
+#define INPUT_FILENAME "in"
+#define OUTPUT_FILENAME "out"
 
 double cpu_time = 20, comp_result;
 
-// do about .5 seconds of computing
+// do a billion floating-point ops
 // (note: I needed to add an arg to this;
 // otherwise the MS C++ compiler optimizes away
 // all but the first call to it!)
 //
-static double do_some_computing(int foo) {
+static double do_a_giga_flop(int foo) {
     double x = 3.14159*foo;
     int i;
-    for (i=0; i<50000000; i++) {
+    for (i=0; i<500000000; i++) {
         x += 5.12313123;
         x *= 0.5398394834;
     }
     return x;
 }
 
-int convert_file(char* in, char* out) {
-    char input_path[1024], output_path[1024];
-    char buf[256];
-
-    boinc_resolve_filename(in, input_path, sizeof(input_path));
-    FILE* infile = boinc_fopen(input_path, "r");
-    if (!infile) {
-        fprintf(stderr,
-            "%s Couldn't find input file, resolved name %s.\n",
-            boinc_msg_prefix(buf, sizeof(buf)), input_path
-        );
-        return -1;
-    }
-
-    boinc_resolve_filename(out, output_path, sizeof(output_path));
-    FILE* outfile = boinc_fopen(output_path, "w");
-    if (!outfile) {
-        fprintf(stderr,
-            "%s Couldn't find output file, resolved name %s.\n",
-            boinc_msg_prefix(buf, sizeof(buf)), output_path
-        );
-        return -1;
-    }
-
-    while (1) {
-        char c = fgetc(infile);
-        if (c == EOF) break;
-        c = toupper(c);
-        fputc(c, outfile);
-    }
-
-    fclose(infile);
-    fclose(outfile);
-    return 0;
-}
-
 int main(int argc, char **argv) {
-    char buf[256];
-    int retval = boinc_init();
+    int i;
+    int c, nchars = 0, retval;
+    char input_path[512], output_path[512], buf[256];
+    MFILE out;
+    FILE* state, *infile;
+
+    for (i=0; i<argc; i++) {
+        if (strstr(argv[i], "cpu_time")) {
+            cpu_time = atof(argv[++i]);
+        }
+    }
+
+    retval = boinc_init();
     if (retval) {
         fprintf(stderr, "%s boinc_init returned %d\n",
             boinc_msg_prefix(buf, sizeof(buf)), retval
@@ -91,25 +80,62 @@ int main(int argc, char **argv) {
         exit(retval);
     }
 
-    for (int i=1; i<argc; i++) {
-        if (!strcmp(argv[i], "--cpu_time")) {
-            cpu_time = atof(argv[++i]);
-        } else {
-            retval = convert_file(argv[i], argv[i+1]);
-            if (retval) exit(-1);
-            i++;
-        }
+    // open the input file (resolve logical name first)
+    //
+    boinc_resolve_filename(INPUT_FILENAME, input_path, sizeof(input_path));
+    infile = boinc_fopen(input_path, "r");
+    if (!infile) {
+        fprintf(stderr,
+            "%s Couldn't find input file, resolved name %s.\n",
+            boinc_msg_prefix(buf, sizeof(buf)), input_path
+        );
+        exit(-1);
+    }
+
+    // open the output file (resolve logical name first)
+    //
+    boinc_resolve_filename(OUTPUT_FILENAME, output_path, sizeof(output_path));
+    retval = out.open(output_path, "wb");
+    if (retval) {
+        fprintf(stderr, "%s APP: upper_case output open failed:\n",
+            boinc_msg_prefix(buf, sizeof(buf))
+        );
+        fprintf(stderr, "%s resolved name %s, retval %d\n",
+            boinc_msg_prefix(buf, sizeof(buf)), output_path, retval
+        );
+        perror("open");
+        exit(1);
+    }
+
+    // main loop - read characters, convert to UC, write
+    //
+    for (i=0; ; i++) {
+        c = fgetc(infile);
+
+        if (c == EOF) break;
+        c = toupper(c);
+        out._putchar(c);
+        nchars++;
+    }
+
+    retval = out.flush();
+    if (retval) {
+        fprintf(stderr, "%s APP: upper_case flush failed %d\n",
+            boinc_msg_prefix(buf, sizeof(buf)), retval
+        );
+        exit(1);
     }
 
     // burn up some CPU time if needed
     //
     if (cpu_time) {
         double start = dtime();
-        for (int i=0; ; i++) {
+        for (i=0; ; i++) {
             double e = dtime()-start;
             if (e > cpu_time) break;
-            comp_result = do_some_computing(i);
+            comp_result = do_a_giga_flop(i);
         }
     }
+    boinc_fraction_done(1);
     boinc_finish(0);
 }
