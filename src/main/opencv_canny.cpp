aa7bf7b11e838ca1f9d0f3f27c5af68727abb1f1
rodrigo.bruno <istple_vmr@gschembra3.diit.unict.it>
2015-04-22 00:38:10 +0000

    Updated makefile and canny.cpp to compile on planetlab.

--- src/main/opencv_canny.cpp
+++ src/main/opencv_canny.cpp
@@ -1,7 +1,8 @@
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <stdlib.h>
+#include "opencv/cv.h"
+#include "opencv/highgui.h"
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 
 using namespace cv;
 
